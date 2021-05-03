ce24dc282538c45959aadc43845621e7d3d96252
Sanjeev Kannan <49632661+Sanjeeev-K@users.noreply.github.com>
2021-05-03 17:22:31 -0400

    Update logger.py

--- logger.py
+++ logger.py
@@ -14,6 +14,12 @@ class Logger(object):
             name: Name of the sub-folder.
 
         """
+        # self.acc_var = tf.Variable(0, dtype=tf.float32)
+        # self.acc_tag = "acc"
+        # self.acc_summ = tf.compat.v1.summary.scalar(self.acc_tag, self.acc_var)
+        self.sess = tf.compat.v1.Session()
+
+
         if name is None:
             name = "temp"
         self.name = name
@@ -22,12 +28,10 @@ class Logger(object):
                 os.makedirs(os.path.join(log_dir, name))
             except:
                 pass
-            self.writer = tf.summary.create_file_writer(os.path.join(
-                log_dir, name),
-                                                        filename_suffix=name)
+            # self.writer = tf.summary.create_file_writer(os.path.join(
+            self.writer = tf.compat.v1.summary.FileWriter(os.path.join(log_dir, name),filename_suffix=name)
         else:
-            self.writer = tf.summary.create_file_writer(log_dir,
-                                                        filename_suffix=name)
+            self.writer = tf.compat.v1.summary.FileWriter(log_dir,filename_suffix=name)
 
     def scalar_summary(self, tag: str, value: float, step: int):
         """Log a scalar variable.
@@ -38,5 +42,10 @@ class Logger(object):
             step: Iteration step number.
 
         """
-        with self.writer.as_default():
-            tf.summary.scalar(name=tag, data=value, step=step)
+        # with self.writer.as_default():
+        #     tf.summary.scalar(name=tag, data=value, step=step)
+        # self.sess.run( self.acc_var.assign(value) )
+        # self.sess.run( self.acc_tag.assign(tag) 
+        acc_summ = tf.compat.v1.summary.scalar( tag, value )
+
+        self.writer.add_summary( self.sess.run( acc_summ ), step)
